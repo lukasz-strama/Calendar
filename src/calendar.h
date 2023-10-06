8aa35810b391e06ba2874dee13aaf8109bcb6fa5
strama <lukasz-strama@outlook.com>
2023-10-06 15:45:55 +0200

    accept command-line arguments (argc and argv).
    accept command-line arguments (argc and argv)..

--- src/calendar.h
+++ src/calendar.h
@@ -4,4 +4,7 @@
 // Function to display the calendar for a specific month
 void displayCalendar(int selected_month, int current_month, int current_day);
 
+// Add an extern declaration for the month_names array
+extern const char *month_names[];
+
 #endif
--- src/main.c
+++ src/main.c
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
+#include <string.h> // for string comparison
 
-int main() {
+int main(int argc, char *argv[]) {
     int selected_month = 0; // Variable to store the selected month (1-12)
     int current_month = 0;  // Variable to store the current month (1-12)
 
@@ -21,93 +22,121 @@ int main() {
     DWORD fdwMode = ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT;
     SetConsoleMode(hStdin, fdwMode);
 
-    while (1) {
-        // Clear the screen for better readability 
-        system("cls");
-
-        // Display a menu to choose a month
-        printf("Choose a month:\n");
-
-        int highlight_month = 1; // The initially highlighted month
-
-        while (1) {
-            // Clear the menu area before redrawing
-            COORD menuPosition = {0, 3}; // Adjust the position as needed
-            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), menuPosition);
+    // Check for command-line arguments
+    if (argc > 1) {
+        // Check if a month argument is provided
+        if (argc == 2) {
+            char *month_arg = argv[1];
+            // Compare the argument to month names and set selected_month accordingly
             for (int month = 1; month <= 12; month++) {
-                if (month == current_month) {
-                    // Highlight the current month in red
-                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
-                }
-
-                if (month == highlight_month) {
-                    printf("-> ");
-                } else {
-                    printf("   ");
-                }
-
-                // Print month names here
-                switch (month) {
-                    case 1: printf("January"); break;
-                    case 2: printf("February"); break;
-                    case 3: printf("March"); break;
-                    case 4: printf("April"); break;
-                    case 5: printf("May"); break;
-                    case 6: printf("June"); break;
-                    case 7: printf("July"); break;
-                    case 8: printf("August"); break;
-                    case 9: printf("September"); break;
-                    case 10: printf("October"); break;
-                    case 11: printf("November"); break;
-                    case 12: printf("December"); break;
-                    default: printf("Unknown");
-                }
-
-                if (month == current_month) {
-                    // Reset text color
-                    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-                }
-
-                printf("\n");
-            }
-
-            // Check for keyboard input
-            if (_kbhit()) {
-                int key = _getch();
-                if (key == 224) {
-                    key = _getch();
-                    if (key == 80) { // Down arrow
-                        highlight_month = (highlight_month % 12) + 1; // Move to the next month
-                    } else if (key == 72) { // Up arrow
-                        highlight_month = ((highlight_month - 2 + 12) % 12) + 1; // Move to the previous month
-                    }
-                } else if (key == 13) { // Enter key
-                    selected_month = highlight_month;
-                    break; // Exit the loop if Enter is pressed
+                if (stricmp(month_arg, month_names[month]) == 0) {
+                    selected_month = month;
+                    break;
                 }
             }
-
-            Sleep(50); // Add a small delay to avoid high CPU usage
         }
+    }
 
-        if (selected_month == 0) {
-            break; // Exit the loop if 0 is entered
-        }
-
-        // Clear the screen again before displaying the calendar
+    if (selected_month != 0) {
+        // Clear the screen before displaying the calendar
         system("cls");
 
         // Display the calendar for the selected month
         displayCalendar(selected_month, current_month, current_day);
 
-        // Clear the input buffer
-        while (_kbhit()) {
-            _getch();
-        }
-
         // Add a pause to wait for a key press
-        printf("\nPress any key to back...");
+        printf("\nPress any key to continue...");
         _getch(); // Wait for user input
+    } else {
+        // Run the interactive menu loop
+        while (1) {
+            // Clear the screen for better readability 
+            system("cls");
+
+            // Display a menu to choose a month
+            printf("Choose a month:\n");
+
+            int highlight_month = 1; // The initially highlighted month
+
+            while (1) {
+                // Clear the menu area before redrawing
+                COORD menuPosition = {0, 3}; // Adjust the position as needed
+                SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), menuPosition);
+                for (int month = 1; month <= 12; month++) {
+                    if (month == current_month) {
+                        // Highlight the current month in red
+                        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_INTENSITY);
+                    }
+
+                    if (month == highlight_month) {
+                        printf("-> ");
+                    } else {
+                        printf("   ");
+                    }
+
+                    // Print month names here
+                    switch (month) {
+                        case 1: printf("January"); break;
+                        case 2: printf("February"); break;
+                        case 3: printf("March"); break;
+                        case 4: printf("April"); break;
+                        case 5: printf("May"); break;
+                        case 6: printf("June"); break;
+                        case 7: printf("July"); break;
+                        case 8: printf("August"); break;
+                        case 9: printf("September"); break;
+                        case 10: printf("October"); break;
+                        case 11: printf("November"); break;
+                        case 12: printf("December"); break;
+                        default: printf("Unknown");
+                    }
+
+                    if (month == current_month) {
+                        // Reset text color
+                        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+                    }
+
+                    printf("\n");
+                }
+
+                // Check for keyboard input
+                if (_kbhit()) {
+                    int key = _getch();
+                    if (key == 224) {
+                        key = _getch();
+                        if (key == 80) { // Down arrow
+                            highlight_month = (highlight_month % 12) + 1; // Move to the next month
+                        } else if (key == 72) { // Up arrow
+                            highlight_month = ((highlight_month - 2 + 12) % 12) + 1; // Move to the previous month
+                        }
+                    } else if (key == 13) { // Enter key
+                        selected_month = highlight_month;
+                        break; // Exit the loop if Enter is pressed
+                    }
+                }
+
+                Sleep(50); // Shorter sleep duration for smoother interaction
+            }
+
+            if (selected_month == 0) {
+                break; // Exit the loop if 0 is entered
+            }
+
+            // Clear the screen again before displaying the calendar
+            system("cls");
+
+            // Display the calendar for the selected month
+            displayCalendar(selected_month, current_month, current_day);
+
+            // Clear the input buffer
+            while (_kbhit()) {
+                _getch();
+            }
+
+            // Add a pause to wait for a key press
+            printf("\nPress any key to back...");
+            _getch(); // Wait for user input
+        }
     }
 
     return 0;
